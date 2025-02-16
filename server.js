bf9a0de557ff559fe69a658b35656a4b76b88ab6
TuNombreDeUsuario <dooswalok@gmail.com>
2025-02-16 12:29:33 -0500

    P

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ const allowedOrigins = [
     "http://127.0.0.1:5500", 
     "http://127.0.0.1:5501", 
     "http://localhost:5500", 
-    "http://localhost:5501", 
+    "http://localhost:5501",
+    'https://cliente-html-git-master-oswaldo-cuestas-projects.vercel.app', 
     "https://generador-toke-git-master-oswaldo-cuestas-projects.vercel.app/"
 ];
 
