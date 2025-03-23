05f0913c7f613d82c23593a9c5a2918d034a2124
TuNombreDeUsuario <dooswalok@gmail.com>
2025-03-23 11:38:54 -0500

    P

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const pool = new Pool({
     database: 'sessions_db',    // Nombre de la base de datos
     password: '132187ok',       // Contraseña del usuario
     port: 5432,                 // Puerto de PostgreSQL
+    max: 10,
+    idleTimeoutMillis: 30000,
 });
 
 // Verificar la conexión a la base de datos al iniciar
@@ -53,6 +55,7 @@ const allowedOrigins = [
     "http://127.0.0.1:5501",
     "http://localhost:5500",
     "http://localhost:5501",
+    "http://127.0.0.1:5502"
 ];
 
 app.use(cors({
